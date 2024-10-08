3e5dec766e18b73c3ddd57174545d0d0eae9e245
juanpa27 <jbellenzier@gmail.com>
2024-10-07 21:40:15 -0300

    creacion de repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css'
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <button className="btn btn-primary">¡Hola Mundo!</button>
+        <button className="btn w-64 rounded-full">Button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
